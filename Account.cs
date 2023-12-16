ec49b2f4356ca49872e4ee3e369a3d25bb1b0adf
juanIgnacio <juan.melgarejobrabo@gmail.com>
2023-12-16 17:56:12 -0300

    First commit

---
+++ Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeBanking
+{
+    internal class Account
+    {
+        public string accountNumber;
+        public string accountOwner;
+        public int money;
+        public string cbu;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Homebanking
+{
+     class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Select an option with a number");
+            Console.WriteLine("1 - Enter account\n" +
+                             "2 - Create account\n" +
+                             "3 - Delete account\n");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+           
+
+
+            while (choice <= 0 || choice >= 4)
+            {
+                Console.WriteLine("invalid option, please try again\n");
+
+                Console.WriteLine("1 - Enter account\n" +
+                                  "2 - Create account\n" +
+                                  "3 - Delete account\n");
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Choice 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Choice 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Choice 3");
+                    break;
+
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
